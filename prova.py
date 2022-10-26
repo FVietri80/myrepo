7bf9c59feb1559118fafd243718e7da610c809d7
FVietri80 <fvietri@highlandsroma.com>
2022-10-26 16:08:50 +0200

    Breaker

--- prova.py
+++ prova.py
@@ -1,6 +1,33 @@
-import turtle
+##### Brick Breaker
+
+import pygame as py
+
+WIDTH,HEIGHT = 800,600
+
+FPS = 60
+
+win = py.display.set_mode((WIDTH,HEIGHT))
+
+py.display.set_captation("Brick Braker")
+
+def draw(win):
+    win.fill("white")
+    py.display.update()
 
 
-turtle.forward(100)
 
-turtle.exitonclick()
+def main() :
+    clock = py.time.Clock()
+    run = True
+    while run:
+        clock.tick(FPS)
+        for event in py.event.get():
+            if event.type = py.QUIT:
+                run = False
+                break
+        draw(win)
+    py.quit()
+    quint()
+
+if __name__ = "__main__":
+    main()
